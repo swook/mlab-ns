64478d83a5903fb55b35e4d2473af0de5ec63784
Seon-Wook Park <seon.wook@swook.net>
2013-04-22 00:19:18 +0900

    Reduce repeated code

--- messabout/main.go
+++ messabout/main.go
@@ -3,7 +3,6 @@ package messabout
 
 import (
 	"fmt"
-	"io"
 	"io/ioutil"
 	"net/http"
 )
@@ -28,37 +27,29 @@ func init() {
 	c = &http.Client{}
 }
 
-func process(r io.Reader) string {
-	out, _ := ioutil.ReadAll(r)
+func reqBase(url string) string {
+	resp, err := c.Get(url)
+	if err != nil {
+		return ""
+	}
+	out, _ := ioutil.ReadAll(resp.Body)
 	return string(out)
 }
 
 // ReqWithMetro performs a lookup request with metro specified
 func ReqWithMetro(toolURL, metro string) (result string) {
 	url := fmt.Sprintf("%s%s&metro=%s", toolURL, DEFARGS, metro)
-	response, err := c.Get(url)
-	if err != nil {
-		return ""
-	}
-	return process(response.Body)
+	return reqBase(url)
 }
 
 // ReqWithMetro performs a lookup request with an IPv4 address specified
 func ReqWithIP(toolURL, ip string) (result string) {
 	url := fmt.Sprintf("%s%s&ip=%s", toolURL, DEFARGS, ip)
-	response, err := c.Get(url)
-	if err != nil {
-		return ""
-	}
-	return process(response.Body)
+	return reqBase(url)
 }
 
 // ReqWithMetro performs a lookup request with country code specified
 func ReqWithCountry(toolURL, country string) (result string) {
 	url := fmt.Sprintf("%s%s&country=%s", toolURL, DEFARGS, country)
-	response, err := c.Get(url)
-	if err != nil {
-		return ""
-	}
-	return process(response.Body)
+	return reqBase(url)
 }
